c0f19b93e96c5062f3d5702c0bc0ca052a1d46b3
jfox <jfox@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-12-20 20:25:37 +0000

    changes to update.semmod(), new edit.semmod(). John
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/sem@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/sem2/R/update.R
+++ pkg/sem2/R/update.R
@@ -1,3 +1,5 @@
+# last modified 2012-12-20 by J. Fox
+
 combineModels <- function (...){
 	UseMethod("combineModels")
 }
@@ -9,14 +11,17 @@ combineModels.semmod <- function(..., warn=TRUE){
 }
 
 update.semmod <- function (object, file = "", ...) {
+	regularizePaths <- function(p) sub(" *-*> *", "->", sub(" *<-* *", "<-", p))
 	delete.model.element <- function(delete.text, old.model, type = "path") {
 		type <- match.arg(type, c("path", "variable", "coefficient"))
+		if (type == "path") delete.text <- regularizePaths(delete.text)
 		col.index <- c(path = 1, variable = 1, coefficient = 2)[type]
 		delete.text <- strip.white(delete.text)
 		old.model <- old.model[-grep(delete.text, strip.white(old.model[, col.index])),]
 		class(old.model) <- "semmod"
 		return(old.model)
 	}
+	object[, 1] <- regularizePaths(object[, 1])
 	modmat <- scan(file = file, what = list(change = "", var1 = "", 
 					var2 = "", var3 = "", var4 = ""), sep = ",", strip.white = TRUE, 
 			comment.char = "#", fill = TRUE)
@@ -41,5 +46,29 @@ update.semmod <- function (object, file = "", ...) {
 			object[, 1:2] <- mapply(function(x, y) gsub(x, y, object[, 1:2]), submat[i, 1], submat[i, 2])
 		}
 	}
+	if ("fix" %in% modmat[, 1]){
+		fixmat <- modmat[which(modmat[, 1] == "fix"), 2:3, drop=FALSE]
+		fixmat[, 1] <- regularizePaths(fixmat[, 1])
+		for (i in 1:nrow(fixmat)){
+			object[which(object[, 1] == fixmat[i, 1]), 2:3] <- c(NA, fixmat[i, 2])
+		}
+	}
+	if ("free" %in% modmat[, 1]){
+		freemat <- modmat[which(modmat[, 1] == "free"), 2:4, drop=FALSE]
+		freemat[, 1] <- regularizePaths(freemat[, 1])
+		for (i in 1:nrow(freemat)){
+			object[which(object[, 1] == freemat[i, 1]), 2:3] <- freemat[i, 2:3]
+		}
+	}
 	removeRedundantPaths(object)
+}
+
+# the next function is adapted from code contributed by Michael Friendly
+
+edit.semmod <- function(name, ...) {
+	name <- unclass(name)
+	colnames(name) <- c("Path", "Parameter", "StartValue")
+	name <- edit(name)
+	class(name) <- "semmod"
+	name
 }
